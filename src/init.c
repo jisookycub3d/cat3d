fc3272bd72b5fcec83593f225fb9393629dacc63
42kyhan <kyhan@student.42seoul.kr>
2022-09-28 09:32:23 +0900

    fixing git #1

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: kyhan <kyhan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 02:28:13 by kyhan             #+#    #+#             */
-/*   Updated: 2022/09/24 20:16:35 by kyhan            ###   ########.fr       */
+/*   Updated: 2022/09/24 21:27:00 by kyhan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,4 @@ void	init_game(t_game *game)
 {
 	ft_memset(game, 0, sizeof(t_game));
 	game->mlx = mlx_init();
-}
-
-void	init_param(t_param *param)
-{
-	ft_memset(param, 0, sizeof(t_param));
 }
--- src/start_game.c
+++ src/start_game.c
@@ -6,38 +6,36 @@
 /*   By: kyhan <kyhan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 19:34:42 by kyhan             #+#    #+#             */
-/*   Updated: 2022/09/24 20:31:27 by kyhan            ###   ########.fr       */
+/*   Updated: 2022/09/24 21:28:28 by kyhan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cat3d.h"
 #include "../include/param.h"
 
-int	key_press(int keycode, t_param *param)
+int	key_press(int keycode, t_game *game)
 {
 	if (keycode == KEY_ESC)
 	{
 		printf("The End\n");
+		mlx_destroy_window(game->mlx, game->win);
 		exit(0);
 	}
 	if (keycode == KEY_A)
-		param->x--;
+		game->param.x--;
 	if (keycode == KEY_D)
-		param->x++;
+		game->param.x++;
 	if (keycode == KEY_S)
-		param->y--;
+		game->param.y--;
 	if (keycode == KEY_W)
-		param->y++;
+		game->param.y++;
 	return (0);
 }
 
 void	start_game(t_game *game)
 {
-	t_param	param;
-
-	init_param(&param);
 	game->win = mlx_new_window(game->mlx, 2560, 1440, "jisookim");
-	mlx_pixel_put(game->mlx, game->win, param.x, param.y, 500);
-	mlx_hook(game->win, 2, 0, key_press, &param);
+
+	mlx_hook(game->win, 2, 0, key_press, &game);
 	mlx_loop(game->mlx);
 }
